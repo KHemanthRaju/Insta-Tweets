f4f6c95808fe76218c24be0d778a9dc643103f05
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-06-23 22:53:01 +0530

    feat: add reducers

---
+++ src/reducers/dataReducer.jsx
@@ -0,0 +1,12 @@
+export const dataReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_ALL_USERS":
+      return { ...state, users: action.payload };
+    case "SET_ALL_POSTS":
+      return { ...state, posts: action.payload };
+    case "USERS_LOADING":
+      return { ...state, usersLoading: action.payload };
+    default:
+      return state;
+  }
+};
