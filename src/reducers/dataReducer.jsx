f140bf335c1b02681744725754d34c84219f0c83
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-06-24 14:53:57 +0530

    feat: add context

--- src/reducers/dataReducer.jsx
+++ src/reducers/dataReducer.jsx
@@ -6,6 +6,23 @@ export const dataReducer = (state, action) => {
       return { ...state, posts: action.payload };
     case "USERS_LOADING":
       return { ...state, usersLoading: action.payload };
+    case "POSTS_LOADING":
+      return { ...state, postsLoading: action.payload };
+    case "SET_ALL_BOOKMARKS":
+      return { ...state, bookmarks: action.payload };
+    case "SET_USER_POST":
+      return { ...state, userPost: action.payload };
+    case "UPDATE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user._id === action.payload._id ? action.payload : user
+        ),
+      };
+    case "CREATE_NEW_POST":
+      return { ...state, posts: action.payload };
+    case "EDIT_POST":
+      return { ...state, posts: action.payload };
     default:
       return state;
   }
