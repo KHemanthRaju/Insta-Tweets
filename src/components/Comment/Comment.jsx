c1311fc54b2db23905040d7f9daf1a0af4c68d30
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-06-25 15:38:33 +0530

    feat: add Comment

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import CommentItem from "../CommentItem/commentItem";
+// import { AuthContext } from "../../contexts/authContext";
+import "../Comment/Comment.css";
+
+const Comment = ({ post }) => {
+  return (
+    <div>
+      {/* <div className="comment-main-container">
+        <img
+          src={
+            authState?.user?.profileAvatar ||
+            `https://res.cloudinary.com/dqlasoiaw/image/upload/v1686688962/tech-social/blank-profile-picture-973460_1280_d1qnjd.png`
+          }
+          alt="profile-pic"
+        />
+        <div className="comment-input-container">
+          <input
+            type="text"
+            placeholder="Add a comment..."
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+          />
+          <i className="fa-solid fa-paper-plane"></i>
+        </div>
+      </div> */}
+      {post?.comments?.length > 0 ? (
+        post?.comments?.map((comment) => (
+          <CommentItem comment={comment} key={comment._id} />
+        ))
+      ) : (
+        <p>No comments!</p>
+      )}
+    </div>
+  );
+};
+
+export default Comment;
