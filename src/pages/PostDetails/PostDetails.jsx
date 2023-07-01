157fe3cb3679f2c4da777040b2847962f3cc8d10
Koneti Hemanth Raju <rajuhemanth456@gmail.com>
2023-07-01 18:11:32 +0530

    feat: add PostDetails and Profile pages

---
+++ src/pages/PostDetails/PostDetails.jsx
@@ -0,0 +1,98 @@
+import React, { useContext, useEffect, useState } from "react";
+import "../PostDetails/PostDetails.css";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useData } from "../../contexts/dataContext";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { LeftSideBar } from "../../components/LeftSideBar/LeftSideBar";
+import { PostCard } from "../../components/PostCard/PostCard";
+import RightSideBar from "../../components/RightSideBar/RightSideBar";
+
+const PostDetails = () => {
+  document.title = "tech-social | Post Details";
+
+  const [singlePostLoading, setSinglePostLoading] = useState(false);
+  const [postDetails, setPostDetails] = useState({});
+
+  const { dataState } = useData();
+
+  const { postID } = useParams();
+
+  const getPostDetails = async () => {
+    try {
+      setSinglePostLoading(true);
+      const { data, status } = await axios.get(`/api/posts/${postID}`);
+      if (status === 200) {
+        setPostDetails(data?.post);
+        setSinglePostLoading(false);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getPostDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataState.posts]);
+
+  return (
+    <div className="post-details">
+      <Navbar />
+      <div className="post-details-content">
+        <LeftSideBar />
+        <div className="post-details-main">
+          {singlePostLoading ? (
+            <ClipLoader color="var(--primary-dark)" size={60} />
+          ) : (
+            postDetails && (
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <PostCard key={postDetails._id} post={postDetails} />
+                {postDetails?.comments?.length > 0 ? (
+                  <div className="single-page-comment-container">
+                    {postDetails?.comments?.map((comment) => {
+                      const userComment = dataState?.users?.find(
+                        (user) => user.username === comment?.username
+                      );
+                      return (
+                        <div
+                          key={comment._id}
+                          className="post-comment-container"
+                        >
+                          <img
+                            src={
+                              userComment?.profileAvatar ||
+                              `https://res.cloudinary.com/dqlasoiaw/image/upload/v1686688962/tech-social/blank-profile-picture-973460_1280_d1qnjd.png`
+                            }
+                            alt="profile-pic"
+                          />
+                          <div>
+                            <strong>{`${userComment?.firstName} ${userComment?.lastName}`}</strong>
+                            <p className="commnet-text">{comment?.text}</p>
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                ) : null}
+              </div>
+            )
+          )}
+        </div>
+        <div className="rightSideBar">
+          <RightSideBar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostDetails;
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,246 @@
+import React, { useContext, useEffect, useState } from "react";
+import "../Profile/Profile.css";
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { isFollowed } from "../../utils/isFollowed";
+import { unfollowUserHandler } from "../../utils/unfollowUserHandler";
+import { followUserHandler } from "../../utils/followUserHandler";
+import { toast } from "react-toastify";
+import FollowModal from "../../components/FollowModal/followModal";
+import EditProfileModal from "../../components/EditProfileModal/editProfileModal";
+import { getSortedPosts } from "../../utils/sortPosts";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useAuth } from "../../contexts/authContext";
+import { useData } from "../../contexts/dataContext";
+import { Navbar } from "../../components/Navbar/Navbar";
+import { LeftSideBar } from "../../components/LeftSideBar/LeftSideBar";
+import { PostCard } from "../../components/PostCard/PostCard";
+import RightSideBar from "../../components/RightSideBar/RightSideBar";
+
+const Profile = () => {
+  document.title = "tech-social | Profile";
+
+  const { username } = useParams();
+  const { authState } = useAuth();
+  const { dataState, dataDispatch, darkMode } = useData();
+
+  const [profileData, setProfileData] = useState({});
+  const [userPosts, setUserPosts] = useState([]);
+  const [editProfileModal, setEditProfileModal] = useState(false);
+
+  const navigate = useNavigate();
+
+  const [showFollowModal, setShowFollowModal] = useState({
+    show: false,
+    type: "",
+  });
+
+  const getUserDetails = async () => {
+    try {
+      const { data, status } = await axios.get(`/api/users/${username}`);
+      if (status === 200) {
+        setProfileData(data?.user);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getUserPost = async () => {
+    try {
+      const { data, status } = await axios.get(`/api/posts/user/${username}`);
+      if (status === 200) {
+        setUserPosts(data?.posts);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getUserDetails();
+    getUserPost();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [username, dataState?.posts, dataState?.users]);
+
+  return (
+    <div className={`profile ${darkMode && "bgDarkmode"}`}>
+      <Navbar />
+      <div className="profile-content">
+        <LeftSideBar />
+        <div className="profile-main">
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {Object.keys(profileData)?.length > 0 ? (
+              <div
+                className={`profile-container ${
+                  darkMode && "bgSecondaryDarkMode"
+                }`}
+              >
+                {showFollowModal.show && (
+                  <FollowModal
+                    data={
+                      showFollowModal.type === "Following"
+                        ? profileData?.following
+                        : profileData?.followers
+                    }
+                    showFollowModal={showFollowModal}
+                    setShowFollowModal={setShowFollowModal}
+                  />
+                )}
+                {editProfileModal && (
+                  <EditProfileModal
+                    profileData={profileData}
+                    editProfileModal={editProfileModal}
+                    setEditProfileModal={setEditProfileModal}
+                  />
+                )}
+                <div className="profile-container-header">
+                  <div className="profile-name-avatar">
+                    <img
+                      className="avatar"
+                      src={
+                        profileData?.profileAvatar ||
+                        `https://res.cloudinary.com/dqlasoiaw/image/upload/v1686688962/tech-social/blank-profile-picture-973460_1280_d1qnjd.png`
+                      }
+                      alt="avatar"
+                    />
+                    <div>
+                      <h3>
+                        {profileData?.firstName} {profileData?.lastName}
+                      </h3>
+                      <small>@{profileData?.username}</small>
+                    </div>
+                  </div>
+                  {profileData?.username === authState?.user?.username ? (
+                    <button className="edit-button">
+                      <i
+                        className="fa-solid fa-pen fa-md"
+                        onClick={() => setEditProfileModal(true)}
+                      ></i>
+                    </button>
+                  ) : (
+                    <button
+                      className={
+                        isFollowed(dataState?.users, profileData?._id)
+                          ? "following-button"
+                          : "follow-button"
+                      }
+                      onClick={() => {
+                        if (authState?.token) {
+                          if (isFollowed(dataState?.users, profileData?._id)) {
+                            unfollowUserHandler(
+                              authState?.token,
+                              profileData?._id,
+                              dataDispatch
+                            );
+                          } else {
+                            followUserHandler(
+                              authState?.token,
+                              profileData?._id,
+                              dataDispatch
+                            );
+                          }
+                        } else {
+                          toast.error("Please login to follow");
+                          navigate("/login");
+                        }
+                      }}
+                    >
+                      {isFollowed(dataState?.users, profileData?._id)
+                        ? "Following"
+                        : "Follow"}
+                    </button>
+                  )}
+                </div>
+                {profileData?.bio && <p>{profileData?.bio}</p>}
+                {profileData?.website && (
+                  <a
+                    style={{ wordBreak: "break-all" }}
+                    href={profileData?.website}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {profileData?.website}
+                  </a>
+                )}
+                <p>
+                  <i className="fa-solid fa-calendar"></i> Joined{" "}
+                  {` ${new Date(profileData?.createdAt)
+                    .toDateString()
+                    .split(" ")
+                    .slice(1, 4)
+                    .join(" ")}`}
+                </p>
+                <div className="profile-post-follow-details">
+                  <p>
+                    {userPosts?.length}{" "}
+                    {`${userPosts?.length > 1 ? "Posts" : "Post"}`}
+                  </p>
+                  <p
+                    onClick={() =>
+                      setShowFollowModal((prev) => ({
+                        ...prev,
+                        show: true,
+                        type: "Followers",
+                      }))
+                    }
+                  >
+                    {profileData?.followers?.length}{" "}
+                    {`${
+                      profileData?.followers?.length > 1
+                        ? "Followers"
+                        : "Follower"
+                    }`}
+                  </p>
+                  <p
+                    onClick={() =>
+                      setShowFollowModal((prev) => ({
+                        ...prev,
+                        show: true,
+                        type: "Following",
+                      }))
+                    }
+                  >
+                    {profileData?.following?.length}{" "}
+                    {`${
+                      profileData?.following?.length > 1
+                        ? "Followings"
+                        : "Following"
+                    }`}
+                  </p>
+                </div>
+              </div>
+            ) : (
+              <ClipLoader color="var(--primary-dark)" size={50} />
+            )}
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {userPosts.length > 0 &&
+                getSortedPosts(userPosts, "Latest")?.map((post) => (
+                  <PostCard key={post._id} post={post} />
+                ))}
+            </div>
+          </div>
+        </div>
+        <div className="rightSideBar">
+          <RightSideBar />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
